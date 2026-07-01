f6738ca4de85c897f01b76c422504692e7b32d06
ZHA30 <zha303050@gmail.com>
2026-07-01 19:29:10 +0800

    feat(novel): add standalone menu screen
    Open a fullscreen Novel menu from the main menu entry and add Bookshelf, Discover, and Sources placeholders.

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 local _ = dofile((debug.getinfo(1, "S").source:match("^@(.*/)") or "./") .. "i18n/po.lua")
 local InfoMessage = require("ui/widget/infomessage")
+local Menu = require("ui/widget/menu")
 local UIManager = require("ui/uimanager")
 local WidgetContainer = require("ui/widget/container/widgetcontainer")
 
@@ -16,14 +17,58 @@ function Novel:addToMainMenu(menu_items)
         text = _("Novel"),
         sorting_hint = "search",
         callback = function()
-            self.onShowNovelSearch()
+            self:onShowNovelMenu()
         end,
     }
 end
 
-function Novel.onShowNovelSearch()
+function Novel:onShowNovelMenu()
+    if self.novel_menu then
+        UIManager:close(self.novel_menu)
+        self.novel_menu = nil
+    end
+
+    local novel_menu
+    novel_menu = Menu:new{
+        title = _("Novel"),
+        item_table = {
+            {
+                text = _("Bookshelf"),
+                callback = function()
+                    Novel.showUnderDevelopment(_("Bookshelf is under development."))
+                end,
+            },
+            {
+                text = _("Discover"),
+                callback = function()
+                    Novel.showUnderDevelopment(_("Discover is under development."))
+                end,
+            },
+            {
+                text = _("Sources"),
+                callback = function()
+                    Novel.showUnderDevelopment(_("Sources are under development."))
+                end,
+            },
+        },
+        covers_fullscreen = true,
+        is_borderless = true,
+        is_popout = false,
+        title_bar_fm_style = true,
+        close_callback = function()
+            UIManager:close(novel_menu)
+            if self.novel_menu == novel_menu then
+                self.novel_menu = nil
+            end
+        end,
+    }
+    self.novel_menu = novel_menu
+    UIManager:show(self.novel_menu)
+end
+
+function Novel.showUnderDevelopment(message)
     UIManager:show(InfoMessage:new{
-        text = _("Novel online search is under development."),
+        text = message,
     })
 end
 
